c9cbc3306f77ec4cb3e9ee21d1829c836fb2b1ff
yelo <zhihuzeye@gmail.com>
2017-08-16 01:47:41 +0800

    use two spaces instead of tabs

--- src/plantuml.js
+++ src/plantuml.js
@@ -5,31 +5,31 @@ var LANG = 'plantuml'
 var SELECTOR = 'pre[data-lang="' + LANG + '"]'
 
 export function plant (content) {
-	content = style + content
-	return '<img src="http://www.plantuml.com/plantuml/svg/' + encode(content) + '" />'
+  content = style + content
+  return '<img src="http://www.plantuml.com/plantuml/svg/' + encode(content) + '" />'
 }
 
 export function replace (content, selector) {
-	var dom = window.Docsify.dom
-	var $ = dom.create('span', content)
+  var dom = window.Docsify.dom
+  var $ = dom.create('span', content)
 
-	if (!$.querySelectorAll) {
-		return content
-	}
+  if (!$.querySelectorAll) {
+    return content
+  }
 
-	(dom.findAll($, selector) || []).forEach(function (element) {
-		var parent = element.parentNode
-		var planted = dom.create('p', plant(element.innerText))
-		if (parent) {
-			planted.dataset.lang = LANG
-			element.parentNode.replaceChild(planted, element)
-		}
-	})
-	return $.innerHTML
+  (dom.findAll($, selector) || []).forEach(function (element) {
+    var parent = element.parentNode
+    var planted = dom.create('p', plant(element.innerText))
+    if (parent) {
+      planted.dataset.lang = LANG
+      element.parentNode.replaceChild(planted, element)
+    }
+  })
+  return $.innerHTML
 }
 
 export function install (hook) {
-	hook.afterEach(function (content) {
-		return replace(content, SELECTOR)
-	})
+  hook.afterEach(function (content) {
+    return replace(content, SELECTOR)
+  })
 }
