b4477f2b0b3718b5b260f2a57cf0e5876857850e
Gerrit-Jan Lubbertsen <g.lubbertsen@afas.nl>
2020-05-11 12:13:18 +0200

    Added support for relative links to other documents (syntax starts with $)

--- src/plantuml.js
+++ src/plantuml.js
@@ -6,11 +6,36 @@ var SELECTOR = 'pre[data-lang="' + LANG + '"]'
 
 export function plant (content, config) {
   var content = skin(config.skin) + content
-  var svgUrl = (config.serverPath || '//www.plantuml.com/plantuml/svg/') + encode(content)
+  var urlPrefix = (config.serverPath || '//www.plantuml.com/plantuml/svg/')
   if (config.renderSvgAsObject) {
+    var svgUrl = urlPrefix + encode(createUrls(content))
     return '<object type="image/svg+xml" data="'+ svgUrl + '" />'
   }
+  var svgUrl = urlPrefix + encode(content)
   return '<img src="' + svgUrl + '" />'
+}
+
+function createUrls(content) {
+  var location = window.location.toString();
+  var currenturl = location.substring(0, location.lastIndexOf('/')+1)
+
+  return content.replace(/\[\[\$(?<path>(\.?\.\/)*)/g, resolvePath)
+
+  // solution taken from docsify codebase
+  function resolvePath(path) 
+  {
+    var segments = path.replace("[[$", currenturl).split('/')
+    var resolved = []
+    for (var i = 0, len = segments.length; i < len; i++) {
+      var segment = segments[i]
+      if (segment === '..') {
+        resolved.pop()
+      } else if (segment !== '.') {
+        resolved.push(segment)
+      }
+    }
+    return '[[' + resolved.join('/')
+  }
 }
 
 export function replace (content, selector, config) {
